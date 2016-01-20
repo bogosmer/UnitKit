0e6e01b01257bfea12d3059dc8886e68c6242155
Bo Gosmer <bg@wallmob.com>
2016-01-20 17:09:26 +0100

    Initial Commit

---
+++ UnitKit/UnitKit.h
@@ -0,0 +1,19 @@
+//
+//  UnitKit.h
+//  UnitKit
+//
+//  Created by Bo Gosmer on 20/01/2016.
+//  Copyright © 2016 Deadlock Baby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UnitKit.
+FOUNDATION_EXPORT double UnitKitVersionNumber;
+
+//! Project version string for UnitKit.
+FOUNDATION_EXPORT const unsigned char UnitKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitKit/PublicHeader.h>
+
+
